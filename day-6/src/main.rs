4cca89ed0a75c393502c0913cb4102f336d35796
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-12-12 23:22:07 +0100

    Start on part 1

--- day-6/src/main.rs
+++ day-6/src/main.rs
@@ -1,3 +1,17 @@
+struct Object {
+    name: String,
+    num_orbits: u32,
+}
+
+impl Object {
+    fn new(name: String) -> Object {
+        Object {
+            name,
+            num_orbits: 0,
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
